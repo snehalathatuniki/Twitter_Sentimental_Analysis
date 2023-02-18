53ba42498f808ff9748372e58e74fc5b4bb746f2
Katta-Gowtham <61149426+Katta-Gowtham@users.noreply.github.com>
2023-02-17 21:54:01 -0500

    Update data_cleaning.py
    code updated to remove duplicates and punctuations, and numbers from data frame

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -7,6 +7,19 @@ duplicates.head(15)
 
 # To remove duplicates
 def remove_duplicates(data):
-    return data.drop_duplicates(subset="tweetID", keep="first")
+    return data.drop_duplicates(subset="tweetID", keep="first") #method used to remove duplicates
 twitter = remove_duplicates(twitter)
 twitter.info()
+
+import re # provides regular expression matching operations.
+def remove_punct(text):
+    text  = "".join([char for char in text if char not in string.punctuation]) 
+    ##string module that contains all punctuation characters
+    ##non-punctuation characters is then joined back together into a string using the join() method
+    text = re.sub('[0-9]+', '', text)
+    ##removes the digits from the string
+    return text
+
+twitter['tweetContext'] = twitter['tweetContext'].apply(lambda x: remove_punct(x))
+#Pandas method that applies a function to each element of a DataFrame.
+twitter.head(12)
