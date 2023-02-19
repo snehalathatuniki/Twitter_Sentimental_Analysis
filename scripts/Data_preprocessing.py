ebc2466856c0a06b76551cdddb66fe5da9e254c6
snehalatha tuniki <43737913+snehalathatuniki@users.noreply.github.com>
2023-02-19 00:14:01 -0500

    Added code for stop words removal

--- scripts/Data_preprocessing.py
+++ scripts/Data_preprocessing.py
@@ -1,5 +1,18 @@
 from nltk.util import ngrams
+import nltk
+from nltk.corpus import stopwords
 
 def tokenize(text):
+    # Generates bi-gram tuples using the NLTK library's ngrams function
+    # Input: tweet and the size of the n-grams
     ngram = ngrams(text.split(),2)
+    # Joins the bi-grams into strings and returns them as a list
     return  [" ".join(gram) for gram in ngram]
+
+def remove_stop_words(tokens):
+    # Creates a set of stop words for the English language using the 'stopwords' module
+    stop_words = set(stopwords.words('english'))
+    # Create a new list of tokens that do not appear in the set of stop words using list comprehension
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # Return the filtered tokens list
+    return filtered_tokens
